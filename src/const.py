9140c90011408ffec74a961a0fbb272912e9d084
Alejandro <alejandro.larin.orellana@gmail.com>
2025-02-04 06:56:43 +0100

    feat(config.py):add ner config

--- src/const.py
+++ src/const.py
@@ -1,2 +1,2 @@
-PATH_DOWNLOADS="./src/output"
+PATH="./output"
 URL="https://www.youtube.com/watch?v=jzi6RNVEOtA&list=PLIOSCF4_abZT8jgFlzqw5cQkMh-eSNuqr"
--- src/main.py
+++ src/main.py
@@ -1,12 +1,24 @@
-from pytubefix import YouTube
+from pytubefix import Playlist
+from colorama import Fore, Style
 from mod.config import Config
 import const
 
 
 config = Config()
-config.clear()
-config.pytubefix.innertube.replace_token_verifier()
+
+def init_config():
+    print(Style.BRIGHT)
+    config.pytubefix.innertube.replace_token_verifier()
+    config.create_output(const.PATH)
+
+
+def download_playlist(url,path):
+    print('descargando playlist')
+
+if __name__ == '__main__':
+    init_config()
+    download_playlist(const.URL, const.PATH)
+
+Style.RESET_ALL
 config.clear()
 
-yt = YouTube(const.URL,use_po_token=True)
-print(yt.streams)
